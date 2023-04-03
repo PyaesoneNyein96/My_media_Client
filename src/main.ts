4eb9676e1e7ce085e03569f17cd02d7c1bbb3320
pyaesonenyein <pyaesonenyein.1996@gmail.com>
2023-04-04 02:00:36 +0630

    client UI

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Header from './views/Header/header_index.vue';
+import Footer from './views/Footer/footer-index.vue'
+
+
+
+
+const app = createApp(App);
+
+app.component('app-Header', Header);
+app.component('app-footer', Footer);
+
+app.use(store).use(router);
+app.mount('#app');
+// createApp(App).use(store).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home/home-index.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home,
+    alias: ['/', '/home']
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
